723176eb7652cec84210f9d0ba7699c2abbaceb6
agustinnavas <145386817+agustinnavas@users.noreply.github.com>
2023-10-31 20:05:14 +0100

    creando proyecto

---
+++ practica-lunes2/src/test/java/com/example/practicalunes2/PracticaLunes2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicalunes2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaLunes2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
